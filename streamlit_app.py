6c746a0bb3d7881dbfd0946a6cf4c5532ef09cd9
jstnnwbrr <jstnnwbrr@gmail.com>
2025-11-27 11:48:38 -0700

    Swaps out template code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+from st_excel_table import Table
+
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Streamlit-Excel-Table")
+
+data = [
+    {"id": "hoge", "x": 5.77, "y": 8.85, "color": "red"},
+    {"id": "hogedb", "x": 15.77, "y": 18.85, "color": "red"},
+    {"id": "hogeba", "x": 25.77, "y": 28.85, "color": "red"},
+    {"id": "hogeas", "x": 35.77, "y": 38.85, "color": "red"},
+]
+
+table = Table(data, columns)
+table
+
+st.dataframe(table)
